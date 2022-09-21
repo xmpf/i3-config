40fa910537821544a27c6f0992872aecfe8e9469
ishtar <ishtar@local.ovf>
2022-09-21 15:34:47 +0300

    2022-09-21: i3wm config

---
+++ tools/init-rofi.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+cat << EOF > ~/.cache/rofi-3.runcache
+90 google-chrome-stable
+90 firefox
+90 brave
+80 discord
+80 thunderbird
+80 BurpSuiteCommunity
+60 VirtualBox
+50 keepassxc
+40 wireshark
+20 obsidian
+20 nm-applet
+10 lxrandr
+10 pavucontrol
+10 code
+EOF
